5037352c577c2f66fe93c43358fc172048571b32
upskubra <yildirimk268@gmail.com>
2022-04-12 16:30:51 +0300

    create CatDetailFragment && FavCatFragment

---
+++ app/src/main/java/com/example/catbreeds/CatDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.example.catbreeds
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CatDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cat_detail, container, false)
+    }
+
+}
