0b9336a34f462bb61697001e9c987e59c775cb5b
upskubra <yildirimk268@gmail.com>
2022-04-13 01:41:16 +0300

    design CatDetailFragment

--- app/src/main/java/com/example/catbreeds/view/CatDetailFragment.kt
+++ app/src/main/java/com/example/catbreeds/view/CatDetailFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.catbreeds.R
+import com.example.catbreeds.databinding.FragmentCatDetailBinding
 
 
 class CatDetailFragment : Fragment() {
 
+    // set data binding
+    private lateinit var binding: FragmentCatDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +24,17 @@ class CatDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cat_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cat_detail, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.catDetailBack.setOnClickListener {
+            val action = CatDetailFragmentDirections.actionCatDetailFragmentToCatListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
