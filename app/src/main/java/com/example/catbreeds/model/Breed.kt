e6d0a17bc8b9c99fdd696f9d185d354898446070
upskubra <yildirimk268@gmail.com>
2022-04-13 10:18:34 +0300

    implementation retrofit

--- app/src/main/java/com/example/catbreeds/model/Breed.kt
+++ app/src/main/java/com/example/catbreeds/model/Breed.kt
@@ -1,13 +1,24 @@
 package com.example.catbreeds.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Breed(
+    @SerializedName("description")
     val description: String,
+    @SerializedName("dog_friendly")
     val dog_friendly: Int,
+    @SerializedName("id")
     val id: String,
+    @SerializedName("life_span")
     val life_span: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("origin")
     val origin: String,
+    @SerializedName("reference_image_id")
     val reference_image_id: String,
+    @SerializedName("temperament")
     val temperament: String,
+    @SerializedName("wikipedia_url")
     val wikipedia_url: String
 )
--- app/src/main/java/com/example/catbreeds/model/CatModel.kt
+++ app/src/main/java/com/example/catbreeds/model/CatModel.kt
@@ -1,9 +1,16 @@
 package com.example.catbreeds.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CatModel(
+    @SerializedName("breeds")
     val breeds: List<Breed>,
+    @SerializedName("height")
     val height: Int,
+    @SerializedName("id")
     val id: String,
+    @SerializedName("url")
     val url: String,
+    @SerializedName("width")
     val width: Int
 )
