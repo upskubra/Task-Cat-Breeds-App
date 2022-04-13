81077096fee53184593adf29381c6823543dbe80
upskubra <yildirimk268@gmail.com>
2022-04-14 01:13:22 +0300

    refactor Data Classes

--- app/src/main/java/com/example/catbreeds/model/CatModel.kt
+++ app/src/main/java/com/example/catbreeds/model/CatModel.kt
@@ -2,15 +2,4 @@ package com.example.catbreeds.model
 
 import com.google.gson.annotations.SerializedName
 
-data class CatModel(
-    @SerializedName("breeds")
-    val breeds: List<Breed>,
-    @SerializedName("height")
-    val height: Int,
-    @SerializedName("id")
-    val id: String,
-    @SerializedName("url")
-    val url: String,
-    @SerializedName("width")
-    val width: Int
-)
+class CatModel : ArrayList<CatModelItem>()
---
+++ app/src/main/java/com/example/catbreeds/model/İmage.kt
@@ -0,0 +1,15 @@
+package com.example.catbreeds.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class İmage(
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("width")
+    val width: Int
+)
