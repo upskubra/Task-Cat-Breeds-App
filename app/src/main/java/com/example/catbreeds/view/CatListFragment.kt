d923ceb27d348ed7fcd213a68db0fc38303e24cb
upskubra <yildirimk268@gmail.com>
2022-04-15 00:30:09 +0300

    initialize CatListViewModel with coroutines

--- app/src/main/java/com/example/catbreeds/view/CatListFragment.kt
+++ app/src/main/java/com/example/catbreeds/view/CatListFragment.kt
@@ -7,20 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.catbreeds.API.RetrofitBuilder
 import com.example.catbreeds.adapter.CatListRecyclerAdapter
 import com.example.catbreeds.R
 import com.example.catbreeds.databinding.FragmentCatListBinding
 import com.example.catbreeds.model.CatModel
-import com.example.catbreeds.model.CatModelItem
+import com.example.catbreeds.viewmodel.CatListViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class CatListFragment : Fragment() {
     private lateinit var binding: FragmentCatListBinding
-    private val retrofitBuilder = RetrofitBuilder()
+    private lateinit var viewModel: CatListViewModel
+    private val catAdapter = CatListRecyclerAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,17 +39,29 @@ class CatListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        retrofitBuilder.getData().enqueue(object : Callback<CatModel> {
-            override fun onFailure(call: Call<CatModel>, t: Throwable) {
-                Log.d("NotWorking", t.message!!)
-            }
-            override fun onResponse(
-                call: Call<CatModel>,
-                response: Response<CatModel>
-            ) {
-                binding.recyclerView.layoutManager = LinearLayoutManager(context)
-                binding.recyclerView.adapter =
-                    CatListRecyclerAdapter(response.body()!!.toList() as ArrayList<CatModelItem>)
+        viewModel = ViewModelProvider(this).get(CatListViewModel::class.java)
+        viewModel.getCats()
+        observeLiveData()
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = catAdapter
+    }
+        //retrofitBuilder.getData().enqueue(object : Callback<CatModel> {
+        //override fun onFailure(call: Call<CatModel>, t: Throwable) {
+        //    Log.d("NotWorking", t.message!!)
+    // override fun onResponse(
+    //    call: Call<CatModel>,
+    //  response: Response<CatModel>
+    //) {
+    //  var catAdapter = binding.recyclerView.adapter
+    //    CatListRecyclerAdapter(response.body()!!.toList() as ArrayList<CatModelItem>)
+    // })
+    //}
+
+    private fun observeLiveData() {
+        viewModel.cats.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            it.let { catModelItem ->
+                binding.recyclerView.visibility = View.VISIBLE
+                catAdapter.updateCatList(catModelItem)
             }
         })
     }
--- app/src/main/java/com/example/catbreeds/viewmodel/CatListViewModel.kt
+++ app/src/main/java/com/example/catbreeds/viewmodel/CatListViewModel.kt
@@ -1,12 +1,29 @@
 package com.example.catbreeds.viewmodel
 
+import android.media.Image
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.catbreeds.R
 import com.example.catbreeds.model.CatModelItem
 
 class CatListViewModel : ViewModel() {
 
-    private val cats = MutableLiveData<List<CatModelItem>>()
+    val cats = MutableLiveData<List<CatModelItem>>()
+    // sample data
+    val cat1 = CatModelItem(
+        " ", 1,
+        " ", " ", "", "",
+        " ", " ", " "
+    )
+    val cat2 = CatModelItem(
+        " ", 1,
+        " ", " ", "", "",
+        " ", " ", " "
+    )
 
+    val catsListSample : List<CatModelItem> = listOf(cat1, cat2)
 
+    fun getCats() {
+        cats.value = catsListSample
+    }
 }
