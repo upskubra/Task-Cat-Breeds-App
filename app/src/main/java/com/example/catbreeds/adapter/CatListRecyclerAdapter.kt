9656c897661d8be06c3efde448207e4cfb10bf16
upskubra <yildirimk268@gmail.com>
2022-04-18 16:37:10 +0300

    refactor & code cleaning in rv adapter

--- app/src/main/java/com/example/catbreeds/adapter/CatListRecyclerAdapter.kt
+++ app/src/main/java/com/example/catbreeds/adapter/CatListRecyclerAdapter.kt
@@ -2,65 +2,45 @@ package com.example.catbreeds.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.catbreeds.R
 import com.example.catbreeds.databinding.RecyclerViewItemBinding
 import com.example.catbreeds.model.CatModelItem
 import com.example.catbreeds.view.CatListFragmentDirections
 
-class CatListRecyclerAdapter(private val list: ArrayList<CatModelItem>) :
-    RecyclerView.Adapter<CatListRecyclerAdapter.ViewHolder>() {
+class CatListRecyclerAdapter(var list: ArrayList<CatModelItem>) :
+    RecyclerView.Adapter<CatListRecyclerAdapter.CatViewHolder>() {
 
-    class ViewHolder(itemView: RecyclerViewItemBinding) :
-        RecyclerView.ViewHolder(itemView.root) {
 
-        private val textView: TextView = itemView.recyclerItemName
-        private val image: ImageView = itemView.recyclerItemImageView
-        fun bind(cat: CatModelItem) {
-            textView.text = cat.name
-            try {
-                val imageUrl: String = cat.image.url
-                Glide.with(this.itemView.context).load(imageUrl).into(image)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
+    class CatViewHolder(var view: RecyclerViewItemBinding) : RecyclerView.ViewHolder(view.root) {
+
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-
+        //val view = inflater.inflate(R.layout.item_country,parent,false)
         val view = DataBindingUtil.inflate<RecyclerViewItemBinding>(
             inflater,
             R.layout.recycler_view_item,
             parent,
             false
         )
-        return ViewHolder(view)
+        return CatViewHolder(view)
+
     }
 
-    override fun getItemCount(): Int = list.size
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(list[position])
-        holder.itemView.setOnClickListener {
-            val action = CatListFragmentDirections.actionCatListFragmentToCatDetailFragment(
-                list[position].name,
-                list[position].origin,
-                list[position].image.url,
-                list[position].description,
-                list[position].dogFriendly,
-                list[position].wikipediaUrl,
-                list[position].lifeSpan
-            )
-            it.findNavController().navigate(action)
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+        holder.view.cats = list[position]
+        holder.view.root.setOnClickListener {
+            val action = CatListFragmentDirections.actionCatListFragmentToCatDetailFragment()
+            action.uid = list[position].uid
+            Navigation.findNavController(it).navigate(action)
         }
     }
 
