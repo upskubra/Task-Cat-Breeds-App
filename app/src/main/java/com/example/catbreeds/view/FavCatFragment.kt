48b4cd148f040977c9a334d549e7dfecc8a60135
upskubra <yildirimk268@gmail.com>
2022-04-18 16:59:31 +0300

    back button onclick, viewmodel connected

--- app/src/main/java/com/example/catbreeds/view/FavCatFragment.kt
+++ app/src/main/java/com/example/catbreeds/view/FavCatFragment.kt
@@ -1,14 +1,21 @@
 package com.example.catbreeds.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.catbreeds.R
+import com.example.catbreeds.databinding.FragmentFavCatBinding
+import com.example.catbreeds.viewmodel.FavCatViewModel
 
 
 class FavCatFragment : Fragment() {
+    private lateinit var binding: FragmentFavCatBinding
+    private lateinit var viewModel: FavCatViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +25,20 @@ class FavCatFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fav_cat, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_fav_cat, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(FavCatViewModel::class.java)
+
+        binding.backBtn.setOnClickListener {
+            val action = FavCatFragmentDirections.actionFavCatFragmentToCatListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
