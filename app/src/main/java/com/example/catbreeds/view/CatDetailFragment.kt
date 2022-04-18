434e167763f974a9d24f9ab01042d6c033158021
upskubra <yildirimk268@gmail.com>
2022-04-18 16:34:31 +0300

    refactor & code cleaning with single variable (uid)

--- app/src/main/java/com/example/catbreeds/view/CatDetailFragment.kt
+++ app/src/main/java/com/example/catbreeds/view/CatDetailFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import com.bumptech.glide.Glide
 import com.example.catbreeds.R
 import com.example.catbreeds.databinding.FragmentCatDetailBinding
 import com.example.catbreeds.viewmodel.CatDetailViewModel
@@ -20,13 +19,7 @@ class CatDetailFragment : Fragment() {
     // set data binding
     private lateinit var binding: FragmentCatDetailBinding
     private lateinit var viewModel: CatDetailViewModel
-    private lateinit var catName: String
-    private lateinit var image: String
-    private lateinit var origin: String
-    private lateinit var url: String
-    private lateinit var lifeSpan: String
-    private var dogFriendly: Int? = null
-    private lateinit var description: String
+    private var uid: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,38 +36,29 @@ class CatDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        arguments?.let {
+
+            uid = CatDetailFragmentArgs.fromBundle(it).uid
+        }
         viewModel = ViewModelProvider(this).get(CatDetailViewModel::class.java)
-        viewModel.getCatLiveData()
+        viewModel.getDataFromDatabase(uid)
+
+
         binding.catDetailBack.setOnClickListener {
             val action = CatDetailFragmentDirections.actionCatDetailFragmentToCatListFragment()
             Navigation.findNavController(it).navigate(action)
         }
-        arguments?.let {
-            catName = CatDetailFragmentArgs.fromBundle(it).catName
-            image = CatDetailFragmentArgs.fromBundle(it).catImage
-            dogFriendly = CatDetailFragmentArgs.fromBundle(it).catDogFriendly
-            lifeSpan = CatDetailFragmentArgs.fromBundle(it).catLifeSpan
-            origin = CatDetailFragmentArgs.fromBundle(it).catOrigin
-            description = CatDetailFragmentArgs.fromBundle(it).catDescription
-            url = CatDetailFragmentArgs.fromBundle(it).catWikiUrl
-        }
 
-        getData()
+
+        observeLiveData()
     }
 
-    private fun getData() {
-        binding.catDetailName.text = catName
-        binding.catDetailDog.text = dogFriendly.toString()
-        binding.catDetailLife.text = lifeSpan
-        binding.catDetailOrigin.text = origin
-        binding.catDetailTemperament.text = description
-        binding.catDetailUrl.text = url
-
-        try {
-            val imageUrl = image
-            Glide.with(this).load(imageUrl).into(binding.catDetailImage)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+    private fun observeLiveData() {
+        viewModel.catLiveData.observe(viewLifecycleOwner, Observer {
+            it?.let { cat ->
+                binding.selectedCat = cat
+            }
+        })
     }
 }
