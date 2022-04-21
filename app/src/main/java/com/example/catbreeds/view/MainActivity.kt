0c72ef8946d50c16a013746fc3d98cbbe7f51bdd
upskubra <yildirimk268@gmail.com>
2022-04-21 07:53:55 +0300

    bug fix actionbar

--- app/src/main/java/com/example/catbreeds/view/MainActivity.kt
+++ app/src/main/java/com/example/catbreeds/view/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.toolbar.title = "Cat Breeds"
-        setSupportActionBar(binding.toolbar) }
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.show()
+    }
 
 }
